768116715daefbb1f26011362e638bab9c4577aa
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-09-04 11:46:01 -0400

    Use KSP 2.3.11 with Kotlin 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
