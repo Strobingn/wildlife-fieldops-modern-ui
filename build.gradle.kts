703a6424c9ac90e658ba781a7fc873879caa87a2
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-09-04 11:37:47 -0400

    Bump Kotlin to 2.3.0 so LiteRT-LM 0.16.1 can compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 // Top-level build file for Wildlife FieldOps native Android app
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.0" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
